93211b6acecad1f2ecbc591523fc2a37327cb86b
Diogo Vilela <diogofsvilela@gmail.com>
2019-03-12 17:21:49 +0000

    Add logic of TransactionalGet in Partition class

--- src/DHADVTKV/Partition.java
+++ src/DHADVTKV/Partition.java
@@ -6,15 +6,53 @@ import java.util.Set;
 
 public class Partition {
 
-    public DataObject transactionalGet(int key, int version) {
-        return new DataObject(0,0,0);
+    private KeyValueStorage kv = new KeyValueStorage();
+
+
+    public DataObject transactionalGet(int key, int snapshot) {
+        return onClientTransactionGetRequest(key, snapshot);
     }
 
-    public void prepare(int id, int snapshot, Set<Partition> putPartitions, Set<Partition> getPartitions) {
+    private DataObject onClientTransactionGetRequest(int key, int snapshot) {
 
+        ArrayList<DataObject> tentativeObjectVersions = kv.getTentativeVersions(key);
+        ArrayList<DataObject> committedObjectVersions = kv.getCommittedVersions(key);
+        DataObject snapshotConsistentVersion = selectSnapshotConsistentVersion(snapshot, tentativeObjectVersions, committedObjectVersions);
+
+        return snapshotConsistentVersion;
+    }
+
+    //TODO: Check if instead of Set<Partition> its ArrayList<DataObjects>!!!
+    public void prepare(int transactionId, int snapshot, Set<Partition> puts, Set<Partition> gets) {
     }
 
     public void commit(int id, List<DataObject> gets, ArrayList<DataObject> puts, boolean hasConflicts, int commitTimestamp) {
+    }
+
+
+
+
+    private DataObject selectSnapshotConsistentVersion(int snapshot, ArrayList<DataObject> tentativeObjectVersions, ArrayList<DataObject> commitedObjectVersions) {
+
+        for (DataObject object : tentativeObjectVersions) {
+            if (object.getVersion() <= snapshot) {
+                waitUntilVersionIsCommitted();
+                return object;
+            }
+        }
+
+        for (DataObject object : commitedObjectVersions) {
+            if (object.getVersion() <= snapshot) {
+                return object;
+            }
+        }
+
+        return null;
+    }
+
+    //TODO: This is obviously ONLY a placeholder!!!
+    private void waitUntilVersionIsCommitted() {
+
     }
 
 }
