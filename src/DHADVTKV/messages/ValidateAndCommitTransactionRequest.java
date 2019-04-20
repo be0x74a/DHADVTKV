8a1a2a9a793d4326883435baa1455a97c2df77bf
Diogo Vilela <diogofsvilela@gmail.com>
2019-04-20 23:45:12 +0100

    Add the proposed architecture with lock based client

--- src/DHADVTKV/messages/ValidateAndCommitTransactionRequest.java
+++ src/DHADVTKV/messages/ValidateAndCommitTransactionRequest.java
@@ -1,4 +1,51 @@
 package DHADVTKV.messages;
 
-public class ValidateAndCommitTransactionRequest {
+import DHADVTKV.common.DataObject;
+
+import java.util.List;
+
+public class ValidateAndCommitTransactionRequest extends Message{
+
+    private final long transactionId;
+    private final long snapshot;
+    private final List<DataObject> puts;
+    private final List<DataObject> gets;
+    private final int client;
+    private final int partition;
+    private static final long LENGTH = 2064;
+    private final static long CPU_TIME = 560;
+
+    public ValidateAndCommitTransactionRequest(long transactionId, long snapshot, List<DataObject> puts, List<DataObject> gets, int client, int partition) {
+        super(LENGTH, CPU_TIME);
+        this.transactionId = transactionId;
+        this.snapshot = snapshot;
+        this.puts = puts;
+        this.gets = gets;
+        this.client = client;
+        this.partition = partition;
+    }
+
+    public long getTransactionId() {
+        return transactionId;
+    }
+
+    public long getSnapshot() {
+        return snapshot;
+    }
+
+    public List<DataObject> getPuts() {
+        return puts;
+    }
+
+    public List<DataObject> getGets() {
+        return gets;
+    }
+
+    public int getPartition() {
+        return partition;
+    }
+
+    public int getClient() {
+        return client;
+    }
 }
