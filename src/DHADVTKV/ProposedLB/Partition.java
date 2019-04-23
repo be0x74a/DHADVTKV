1a5d8647ac9ef9db1af786c8f52bfee030dbec10
Diogo Vilela <diogofsvilela@gmail.com>
2019-04-23 17:57:11 +0100

    Finish ProposedLB.Partition and create ProposedLB.PartitionProtocol

--- src/DHADVTKV/ProposedLB/Partition.java
+++ src/DHADVTKV/ProposedLB/Partition.java
@@ -2,9 +2,7 @@ package DHADVTKV.ProposedLB;
 
 import DHADVTKV.common.DataObject;
 import DHADVTKV.common.KeyValueStorage;
-import DHADVTKV.messages.TransactionalGetMessageRequest;
-import DHADVTKV.messages.TransactionalGetMessageResponse;
-import DHADVTKV.messages.ValidateAndCommitTransactionRequest;
+import DHADVTKV.messages.*;
 
 import java.util.List;
 
@@ -12,12 +10,11 @@ public class Partition {
 
     private final Validator validator;
     private KeyValueStorage kv;
-    private long clock = 0;
     private int transactionsDone = 0;
 
     public Partition(long nodeId, int noPartitions, int keyValueStoreSize) {
         this.kv = new KeyValueStorage(nodeId, noPartitions, keyValueStoreSize);
-        this.validator = new Validator();
+        this.validator = new Validator(noPartitions);
     }
 
     public TransactionalGetMessageResponse transactionalGet(TransactionalGetMessageRequest message) {
@@ -29,19 +26,33 @@ public class Partition {
         return new TransactionalGetMessageResponse(obj, message.getPartition(), message.getClient());
     }
 
-    public void validateAndCommit(ValidateAndCommitTransactionRequest message) {
+    public Message validateAndCommit(ValidateAndCommitTransactionRequest message) {
         kv.storeAsTentative(message.getTransactionId(), message.getPuts());
-        validator.leaf_validate(message.getTransactionId(), message.getSnapshot(), message.getGets(), message.getPuts(), message.getClient());
+        Message response = validator.leafValidate(message.getTransactionId(), message.getSnapshot(), message.getGets(), message.getPuts(), message.getClient(), message.getNoPartitionsTouched());
+
+        if (response instanceof ClientValidationResponse) {
+            if (((ClientValidationResponse) response).hasConflicts()) {
+                kv.deleteTentativeVersions(message.getTransactionId(), message.getPuts());
+            } else {
+                kv.commitTentativeVersions(message.getTransactionId(), message.getPuts(), ((ClientValidationResponse) response).getCommitTimestamp());
+            }
+
+            validator.leafCommit(message.getPuts(), ((ClientValidationResponse) response).getCommitTimestamp(), ((ClientValidationResponse) response).hasConflicts());
+        }
+
+        return response;
     }
 
-    public void validationResponse(ValidationResponse message) {
+    public void onTransactionValidationResult(PartitionValidationResponse message) {
         if (message.hasConflicts()) {
             kv.deleteTentativeVersions(message.getTransactionId(), message.getPuts());
         } else {
             kv.commitTentativeVersions(message.getTransactionId(), message.getPuts(), message.getCommitTimestamp());
         }
 
-        validator.leafCommit(message.getGets(), message.getPuts(), message.getCommitTimestamp());
+        validator.leafCommit(message.getPuts(), message.getCommitTimestamp(), message.hasConflicts());
+        this.transactionsDone++;
+        System.out.println(this.transactionsDone);
     }
 
     private DataObject selectSnapshotConsistentVersion(long snapshot, List<DataObject> tentativeObjectVersions, List<DataObject> committedObjectVersions) {
--- src/DHADVTKV/ProposedLB/PartitionProtocol.java
+++ src/DHADVTKV/ProposedLB/PartitionProtocol.java
@@ -1,4 +1,80 @@
 package DHADVTKV.ProposedLB;
 
-public class PartitionProtocol {
+import DHADVTKV.common.Channel;
+import DHADVTKV.messages.*;
+import peersim.config.Configuration;
+import peersim.core.Network;
+import peersim.core.Node;
+import peersim.edsim.EDProtocol;
+import peersim.edsim.EDSimulator;
+
+public class PartitionProtocol implements EDProtocol {
+
+    private final int size;
+    private final int noPartitions;
+    private Partition partition;
+    private String prefix;
+
+    public PartitionProtocol(String prefix) {
+        this.prefix = prefix;
+        this.noPartitions = Configuration.getInt(prefix + "." + "nopartitions");
+        this.size = Configuration.getInt(prefix + "." + "keyvaluestoresize");
+    }
+
+    @Override
+    public void processEvent(Node node, int pid, Object event) {
+    }
+
+    public void processEventCustom(Node node, int pid, Object event) {
+
+        if (partition == null) {
+            int nodeId = Math.toIntExact(node.getID());
+            partition = new Partition(nodeId, this.noPartitions, size);
+        }
+
+        Message response;
+
+        if (event instanceof Message)  {
+            if (!((Message) event).isForCPU()) {
+                ((Message) event).setForCPU(true);
+                EDSimulator.add(((Message) event).getCpuTime(), event, node, pid);
+                return;
+            }
+        } else {
+            throw new RuntimeException("Unknown message type: " + event.getClass().getSimpleName());
+        }
+
+        if (event instanceof TransactionalGetMessageRequest) {
+            TransactionalGetMessageRequest message = (TransactionalGetMessageRequest) event;
+            response = this.partition.transactionalGet(message);
+            sendMessage(message.getClient(), response, pid);
+        } else if (event instanceof ValidateAndCommitTransactionRequest) {
+            ValidateAndCommitTransactionRequest message = (ValidateAndCommitTransactionRequest) event;
+            response = this.partition.validateAndCommit(message);
+            if (response instanceof ClientValidationResponse) {
+                sendMessage(((ClientValidationResponse) response).getClient(), response, pid);
+            } else if (response instanceof NonLeafValidateAndCommitRequest) {
+                sendMessage(noPartitions, response, pid);
+            }
+        } else if (event instanceof PartitionValidationResponse) {
+            PartitionValidationResponse message = (PartitionValidationResponse) event;
+            this.partition.onTransactionValidationResult(message);
+        } else {
+            throw new RuntimeException("Unknown message type: " + event.getClass().getSimpleName());
+        }
+    }
+
+    @Override
+    public Object clone() {
+        return new PartitionProtocol(prefix);
+    }
+
+
+    private void sendMessage(int client, Message message, int pid) {
+
+        Node dst = Network.get(client);
+
+        EDSimulator.add(Channel.putMessageInChannel(message.getLength()), message, dst, pid);
+
+    }
 }
