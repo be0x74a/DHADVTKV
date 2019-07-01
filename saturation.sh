ef0ee810d94a2aa667020bd0bfeab03bf2ddf1aa
Diogo Vilela <diogofsvilela@gmail.com>
2019-07-01 15:42:59 +0100

    100Gbps 12us experiment

--- saturation.sh
+++ saturation.sh
@@ -5,10 +5,9 @@ for i in {0..150..10}
         if [ "$i" -eq "0" ]; then
             i=1
         fi
-
         sed -e 's/TEMPLATE_SIZE/'$(($i+8))'/' < saturation_files/configs/config-2pc.template > saturation_files/configs/config.config
-        java -classpath ./out/production/Sim:./jep-2.3.0.jar:./djep-1.0.0.jar peersim.Simulator saturation_files/configs/config.config >> saturation_files/tpc_data.csv 2>/dev/null 
-        echo ', '$(($i)) >> saturation_files/tpc_data.csv
+        java -classpath ./out/production/Sim:./jep-2.3.0.jar:./djep-1.0.0.jar peersim.Simulator saturation_files/configs/config.config >> saturation_files/results/c13a84107a50d3cb417359d94d671b8b190ae20a/tpc_data.csv 2>/dev/null
+        echo ', '$(($i)) >> saturation_files/results/c13a84107a50d3cb417359d94d671b8b190ae20a/tpc_data.csv
         rm saturation_files/configs/config.config
         for j in {0..30..5}
             do
@@ -16,8 +15,8 @@ for i in {0..150..10}
                     j=1
                 fi
                 sed -e 's/TEMPLATE_SIZE/'$(($i+9))'/' -e 's/BATCH_SIZE/'$j'/' < saturation_files/configs/config-tsb.template > saturation_files/configs/config.config
-                java -classpath ./out/production/Sim:./jep-2.3.0.jar:./djep-1.0.0.jar peersim.Simulator saturation_files/configs/config.config >> saturation_files/tsb_data_$j.csv 2>/dev/null
-                echo ', '$(($i)) >> saturation_files/tsb_data_$j.csv
+                java -classpath ./out/production/Sim:./jep-2.3.0.jar:./djep-1.0.0.jar peersim.Simulator saturation_files/configs/config.config >> saturation_files/results/c13a84107a50d3cb417359d94d671b8b190ae20a/tsb_data_$j.csv 2>/dev/null
+                echo ', '$(($i)) >> saturation_files/results/c13a84107a50d3cb417359d94d671b8b190ae20a/tsb_data_$j.csv
                 rm saturation_files/configs/config.config
 
             done
